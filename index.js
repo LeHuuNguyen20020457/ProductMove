ff06463286016e3dfba469de5c663e4861b14641
LeHuuNguyen20020457 <20020457@vnu.edu.vn>
2022-12-08 10:36:53 +0700

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const db = require('./models');
 
