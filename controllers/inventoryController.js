f0d2f93d7e689c7833e681ba2e8d7427a7231e25
LeHuuNguyen20020457 <20020457@vnu.edu.vn>
2022-12-22 16:58:17 +0700

    xong xuat kho

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,4 +1,4 @@
-const { Inventory } = require('../models');
+const { Inventory , Warehouse, productLine} = require('../models');
 const { Op } = require('sequelize');
 
 class inventoryController {
@@ -30,9 +30,51 @@ class inventoryController {
     //[POST] /inventory/xuatkho
     handleXuatKho(req, res, next) {
         const {selectedAgent, dateXuat, typeXuat, note, tax, promotionalMoney, payments, selectedWarehouse, rowsProductXK} = req.body;
-        
+        // console.log( {selectedAgent, dateXuat, typeXuat, note, tax, promotionalMoney, payments, selectedWarehouse, rowsProductXK})
+        for(let i = 0; i< rowsProductXK.length; i++){
+            Inventory.decrement({
+                inventoryNumber: Number(rowsProductXK[i].amount)
+            }, {
+                where: {
+                   [Op.and]: {
+                        warehouseID: Number(selectedWarehouse.codeWarehouse),
+                        codeProductLine: rowsProductXK[i].codeProductLine
+                   }
+                }
+            })
+            .then(()=> {
+                const dayMonthYear = dateXuat.split('-')
+                var totalMoneys = 0;
+                for(let i = 0; i < rowsProductXK.length; i++){
+                     totalMoneys += Number(rowsProductXK[i].totalPrice.split(' ')[0])
+                }
+                 
+                totalMoneys -= Number(promotionalMoney)
+                totalMoneys += totalMoneys*(Number(tax)/100)
+                
+                res.render('warehouse/phieuxuatkho.hbs', {
+                 selectedAgent,
+                 dayMonthYear,
+                 typeXuat,
+                 tax,
+                 note,
+                 promotionalMoney,
+                 payments,
+                 selectedWarehouse,
+                 rowsProductXK,
+                 totalMoneys,
+                }, function(err, html) {
+                    if(err) res.status(500).send(err)
+                    res.status(200).send(html)
+                })
+            })
+            .catch(() => {
+                console.log('đã update thất bại')
+            })
+        }
 
-
+    //     console.log('da den day')
+    //    res.render('warehouse/phieuxuatkho.hbs')
     }
 
 }
